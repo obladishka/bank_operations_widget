a91ae64733feb818acf484114efc1b8317ee4f36
Goldyban Nastya <nasty-goldyba@ya.ru>
2024-07-04 23:33:39 +0300

    add sort by date function

--- src/processing.py
+++ src/processing.py
@@ -3,3 +3,8 @@ def filter_by_state(
 ) -> list[dict[str, str | int]]:
     """Function that filters information by a specified criteria."""
     return [operation for operation in operations_data if operation.get("state") == state]
+
+
+def sort_by_date(operations_data: list[dict[str, str | int]], parameter: bool = True) -> list[dict[str, str | int]]:
+    """Function that sorts information by date."""
+    return sorted(operations_data, key=lambda date: date["date"], reverse=parameter)
