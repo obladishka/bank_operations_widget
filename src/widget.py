a32eeec829c785e0555ab0cdf6ac3dc875495916
Goldyban Nastya <nasty-goldyba@ya.ru>
2024-07-05 08:57:39 +0300

    update mask_account_card func due to input data format change

--- src/widget.py
+++ src/widget.py
@@ -6,14 +6,15 @@ def mask_account_card(account_card_info: str) -> str:
     info_list: list[str] = account_card_info.split()
 
     if info_list[0].lower() == "счет":
-        masked_info: str = get_mask_account(info_list[-1])
+        return " ".join(info_list[:-1]) + " " + get_mask_account(info_list[-1])
     else:
-        masked_info = get_mask_card_number(info_list[-1])
-
-    return " ".join(info_list[:-1]) + " " + masked_info
+        return " ".join(info_list[:-4]) + " " + get_mask_card_number("".join(info_list[-4:]))
 
 
 def get_data(my_date: str) -> str:
     """Function for date formating."""
     formated_date: list[str] = my_date[:10].split("-")
     return ".".join(formated_date[::-1])
+
+
+print(mask_account_card("Visa Platinum 8990 9221 1366 5229"))
