64210cdc557ebaf7433cd9a1b1aa8abf12056a38
Goldyban Nastya <nasty-goldyba@ya.ru>
2024-07-07 16:41:07 +0300

    add tests for get_mask_account function

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.masks import get_mask_card_number
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def test_get_mask_card_number() -> None:
@@ -32,3 +32,33 @@ def test_get_mask_card_number_wrong_number(card_number: int) -> None:
 )
 def test_get_mask_card_number_nonstandard_number(card_number: int, expected: str) -> None:
     assert get_mask_card_number(card_number) == expected
+
+
+def test_get_mask_account() -> None:
+    assert get_mask_account(73654108430135874305) == "**4305"
+    assert get_mask_account("73654108430135874305") == "**4305"
+
+
+def test_get_mask_account_no_number() -> None:
+    assert get_mask_account("") == "Account number can't be empty"
+
+
+@pytest.mark.parametrize("account_number", [73654, 73654108430135874305123])
+def test_get_mask_account_wrong_number(account_number: int) -> None:
+    with pytest.raises(ValueError, match="Account should be between 6 and 22 digits"):
+        get_mask_account(account_number)
+
+
+@pytest.mark.parametrize(
+    "account_number, expected",
+    [
+        (736541, "**6541"),
+        (7365410, "**5410"),
+        (7365410843013, "**3013"),
+        (73654108430135874305, "**4305"),
+        (736541084301358743051, "**3051"),
+        (7365410843013587430512, "**0512"),
+    ],
+)
+def test_get_mask_account_nonstandard_number(account_number: int, expected: str) -> None:
+    assert get_mask_account(account_number) == expected
