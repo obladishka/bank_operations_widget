221aed416fe3e8979e234921661a889defe8e4fb
Goldyban Nastya <nasty-goldyba@ya.ru>
2024-07-08 23:35:51 +0300

    update sort_by_date function according to tests

--- src/processing.py
+++ src/processing.py
@@ -1,12 +1,27 @@
+from datetime import datetime
+
+
 def filter_by_state(
     operations_data: list[dict[str, str | int]], state: str = "EXECUTED"
 ) -> list[dict[str, str | int]] | str:
     """Function that filters information by a specified criteria."""
-    if state not in [operation.get("state") for operation in operations_data]:
+    if not operations_data:
+        return []
+    elif state not in [operation.get("state") for operation in operations_data]:
         return "State is invalid"
     return [operation for operation in operations_data if operation.get("state") == state]
 
 
 def sort_by_date(operations_data: list[dict[str, str | int]], parameter: bool = True) -> list[dict[str, str | int]]:
     """Function that sorts information by date."""
+    for index, info in enumerate(operations_data):
+        if info.get("date") is None:
+            operations_data.pop(index)
+            print(f"Operation {info.get("id")} date is unknown")
+        else:
+            try:
+                datetime.fromisoformat(str(info.get("date")))
+            except ValueError:
+                operations_data.pop(index)
+                print(f"Operation {info.get("id")} date should be in format YYYY-MM-DD")
     return sorted(operations_data, key=lambda date: date["date"], reverse=parameter)
