a57c314f94e0a20f5411c528ffdf4e743166ceeb
Goldyban Nastya <nasty-goldyba@ya.ru>
2024-07-11 20:12:41 +0300

    add func for returning transactions descriptions

--- src/generators.py
+++ src/generators.py
@@ -1,18 +1,25 @@
 def filter_by_currency(transactions_list, currency):
     """Function that filters transactions in a specified currency."""
-    try:
-        filtered_transactions = list(
-            filter(
-                lambda transaction: transaction.get("operationAmount").get("currency").get("code") == currency,
-                transactions_list,
-            )
-        )
-    except AttributeError:
-        return "Transactions currency is not defined"
-    else:
-        if not transactions_list or len(filtered_transactions) == 0:
-            return "No transactions in such currency"
-        return filter(
-            lambda transaction: transaction.get("operationAmount").get("currency").get("code") == currency,
+    filtered_transactions = list(
+        filter(
+            lambda transaction: transaction.get("operationAmount").get("currency", {}).get("code") == currency,
             transactions_list,
         )
+    )
+
+    if not transactions_list or len(filtered_transactions) == 0:
+        return "No transactions in such currency"
+
+    return iter(filtered_transactions)
+
+
+def transaction_descriptions(transactions_list):
+    """Function that returns description of each transaction in turn."""
+    if not transactions_list:
+        yield "No transactions found"
+
+    descriptions = (
+        transaction.get("description", "Operation description is not defined") for transaction in transactions_list
+    )
+    for description in descriptions:
+        yield description
