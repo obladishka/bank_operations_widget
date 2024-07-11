6a8154988dc6c47858432eb3cda3d5b5e1fd670e
Goldyban Nastya <nasty-goldyba@ya.ru>
2024-07-11 07:10:59 +0300

    add tests for filter_by_currency function

---
+++ tests/test_generators.py
@@ -0,0 +1,66 @@
+from src.generators import filter_by_currency
+
+
+def test_filter_by_currency(transactions):
+    usd_transactions = filter_by_currency(transactions, "USD")
+    assert next(usd_transactions) == {
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702",
+    }
+    assert next(usd_transactions) == {
+        "id": 142264268,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 19708645243227258542",
+        "to": "Счет 75651667383060284188",
+    }
+    rub_transactions = filter_by_currency(transactions, "RUB")
+    assert next(rub_transactions) == {
+        "id": 873106923,
+        "state": "EXECUTED",
+        "date": "2019-03-23T01:09:46.296404",
+        "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 44812258784861134719",
+        "to": "Счет 74489636417521191160",
+    }
+
+
+def test_filter_by_currency_no_transactions(transactions):
+    assert filter_by_currency(transactions, "EUR") == "No transactions in such currency"
+    assert filter_by_currency([], "USD") == "No transactions in such currency"
+
+
+def test_filter_by_currency_no_currency():
+    no_currency_transactions = [
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {
+                "amount": "9824.07",
+            },
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702",
+        },
+        {
+            "id": 142264268,
+            "state": "EXECUTED",
+            "date": "2019-04-04T23:20:05.206878",
+            "operationAmount": {
+                "amount": "79114.93",
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 19708645243227258542",
+            "to": "Счет 75651667383060284188",
+        },
+    ]
+    assert filter_by_currency(no_currency_transactions, "USD") == "Transactions currency is not defined"
