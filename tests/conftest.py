e8f78f0fef10350cffb52e18e06f859dd3a5f1b1
Goldyban Nastya <nasty-goldyba@ya.ru>
2024-07-11 20:10:28 +0300

    modified fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -77,4 +77,12 @@ def transactions() -> list[dict[str, str | int]]:
             "from": "Visa Platinum 1246377376343588",
             "to": "Счет 14211924144426031657",
         },
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+            "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
+            "from": "Visa Platinum 1246377376343588",
+            "to": "Счет 14211924144426031657",
+        },
     ]
