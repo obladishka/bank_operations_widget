18f8588e5a7a67e3f262c4e587affd6a52cc583f
Goldyban Nastya <nasty-goldyba@ya.ru>
2024-07-26 21:56:02 +0300

    add function for returning transaction amount in RUB

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,7 @@
 import json
+from typing import Any
+
+from src.external_api import get_exchange_rate
 
 
 def get_operations_data(file_path: str) -> list[dict]:
@@ -15,3 +18,16 @@ def get_operations_data(file_path: str) -> list[dict]:
 
     except FileNotFoundError:
         return []
+
+
+def get_transaction_amount(transaction: dict[str, Any]) -> float:
+    """Function that returns transaction amount in rubles."""
+    currency = transaction.get("operationAmount").get("currency", {}).get("code")
+    amount = float(transaction.get("operationAmount").get("amount"))
+
+    if currency == "RUB":
+        return amount
+    else:
+        status, result = get_exchange_rate(amount, currency)
+        if status:
+            return result
