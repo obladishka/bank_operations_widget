d95914f4b89ccd870f00503c6f06518483f43758
Goldyban Nastya <nasty-goldyba@ya.ru>
2024-08-03 10:35:58 +0300

    add func for reading data from excel

--- src/read_from_file.py
+++ src/read_from_file.py
@@ -8,11 +8,43 @@ def get_operations_data_from_csv(file_path: str) -> list[dict]:
         operations_dict = df.fillna(0).to_dict(orient="records")
         formated_dict = [
             {
-                "id": i.get("id"),
+                "id": int(i.get("id")),
                 "state": i.get("state"),
                 "date": i.get("date"),
                 "operationAmount": {
-                    "amount": i.get("amount"),
+                    "amount": int(i.get("amount")),
+                    "currency": {"name": i.get("currency_name"), "code": i.get("currency_code")},
+                },
+                "description": i.get("description"),
+                "from": i.get("from"),
+                "to": i.get("to"),
+            }
+            for i in operations_dict
+        ]
+
+        for operation in formated_dict:
+            for key, value in list(operation.items()):
+                if value == 0:
+                    del operation[key]
+
+        return formated_dict
+
+    except FileNotFoundError:
+        return []
+
+
+def get_operations_data_from_excel(file_path: str) -> list[dict]:
+    """Function that returns info about financial transactions from Excel-file in JSON-format."""
+    try:
+        df = pd.read_excel(file_path)
+        operations_dict = df.fillna(0).to_dict(orient="records")
+        formated_dict = [
+            {
+                "id": int(i.get("id")),
+                "state": i.get("state"),
+                "date": i.get("date"),
+                "operationAmount": {
+                    "amount": int(i.get("amount")),
                     "currency": {"name": i.get("currency_name"), "code": i.get("currency_code")},
                 },
                 "description": i.get("description"),
